789dd356cdc8a0167cccb4169838010d25bf1873
Kaushal Sharma <skazi019@gmail.com>
2018-07-08 14:39:20 +0530

    Adding broadcasting feature

--- public/chat.js
+++ public/chat.js
@@ -6,6 +6,7 @@ var message = document.getElementById('message');
     handle = document.getElementById('handle');
     btn = document.getElementById('send');
     output = document.getElementById('output');
+    feedback = document.getElementById('feedback');
 
 //Emit events
 
@@ -16,9 +17,17 @@ btn.addEventListener('click',function(){
   });
 });
 
+message.addEventListener('keypress', function(){
+  socket.emit('typing', handle.value);
+});
 
 //Listen for events
 
 socket.on('chat', function(data){
-  output.innerHTML += '<p><strong>'+ data.handle +':</strong>'+ data.message + '</p>'
+  feedback.innerHTML = '';
+  output.innerHTML += '<p><strong>'+ data.handle +':</strong>'+ data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+  feedback.innerHTML = '<p><em>'+ data + 'is typing a message...</em></p>';
 });
