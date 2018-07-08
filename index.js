00471aad9f53ad4f286502363b1bf499f3de29e0
Kaushal Sharma <skazi019@gmail.com>
2018-07-08 11:43:32 +0530

    Adding 'Public' folder

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var app = express();
 var server = app.listen(4000, function () {
     console.log('Listening to requests on port 4000.');
 });
+
+
+//Static files
+app.use(express.static('public'));
